f77d3c0d29a6896ca3d3ef7eac54f37c678feb56
gagancod3 <gagandeepnayyar@gmail.com>
2025-11-01 21:06:50 +0530

    component lifecycle p1

--- phase_02/components/About.js
+++ phase_02/components/About.js
@@ -1,16 +1,44 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-    return (<div className="about-div">
+
+//* Class-based component
+class About extends React.Component{
+    constructor(props){
+        super(props);
+        console.log('Parent constructor');
+    }
+    componentDidMount(){
+        console.log('Parent componentDidMount');
+    }
+    render(){
+        console.log('Parent render');
+         return (<div className="about-div">
         <h1>About our platform</h1>
         <h2>This is food ordering app</h2>
         {/* Functional Component */}
         <User name={'Gagandeep'} profile={'Developer'}/>
         {/* Class-based component */}
         <UserClass name={'Gagandeep'} profile={'Developer'}/>
+        <UserClass name={'Karan'} profile={'Developer'}/>
+
     </div>
     )
+    }
 }
+
+//* Funcational Component
+// const About = () => {
+//     return (<div className="about-div">
+//         <h1>About our platform</h1>
+//         <h2>This is food ordering app</h2>
+//         {/* Functional Component */}
+//         <User name={'Gagandeep'} profile={'Developer'}/>
+//         {/* Class-based component */}
+//         <UserClass name={'Gagandeep'} profile={'Developer'}/>
+//     </div>
+//     )
+// }
 
 export default About;
--- phase_02/components/UserClass.js
+++ phase_02/components/UserClass.js
@@ -4,16 +4,40 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     // console.log(props);
+        console.log(this.props.name+'child constructor');
+
+    //* This is how we create state variables in class-based components
+    this.state = {
+      count: 0,
+      abcVal: 2,
+    };
+  }
+  componentDidMount(){
+            console.log(this.props.name+'child componentDidMount');
   }
   render() {
-  const {name, profile} = this.props;
+        console.log(this.props.name+'child render');
+
+    const { name, profile } = this.props;
+    const { count, abcVal } = this.state;
 
     return (
+      
       <div className="user-card">
+       <button
+          onClick={() => {
+            //* This is how to update state variable in class-based components
+            this.setState({ count: this.state.count + 1, abcVal: this.state.abcVal + 1 });
+          }}
+        >
+          Click to update count & abcVal
+        </button>
         {/* <h1>{this.props.name}</h1> */}
         <h1>{name}</h1>
         <h2>{profile}</h2>
         <h3>Class component</h3>
+        <h4>state Variable 'count': {count}</h4>
+        <h4>another state Variable 'abcVal': {abcVal}</h4>
       </div>
     );
   }
